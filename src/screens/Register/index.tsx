8ad27e35cc2a1e7575f1c0c68daa9dfc8d198812
Faran42 <faranorphys@gmail.com>
2021-09-21 12:14:57 -0300

    TransactionButton

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Input } from '../../components/Forms/Input/';
 import { Button } from '../../components/Forms/Button/';
+import { TransactionTypeButton } from '../../components/Forms/TransactionTypeButton/';
 
-import { 
+import {
   Header,
   Title,
   Container,
   Form,
-  Fields 
+  Fields,
+  TransactionTypes
 } from './styles';
 
 export function Register() {
+  const [transactionType, setTransactionType] = useState('');
+
+  function handletransactionTypeSelect(type: 'up' | 'down') {
+    setTransactionType(type);
+  }
+
   return (
     <Container>
       <Header>
@@ -19,10 +27,24 @@ export function Register() {
       </Header>
       <Form>
         <Fields>
-          <Input placeholder="Nome"/>
-          <Input placeholder="Preço"/>
+          <Input placeholder="Nome" />
+          <Input placeholder="Preço" />
+          <TransactionTypes>
+            <TransactionTypeButton
+              type="up"
+              title="Income"
+              onPress={() => handletransactionTypeSelect('up')}
+              isActive={transactionType === 'up'}
+            />
+            <TransactionTypeButton
+              type="down"
+              title="Outcome"
+              onPress={() => handletransactionTypeSelect('down')}
+              isActive={transactionType === 'down'}
+            />
+          </TransactionTypes>
         </Fields>
-        <Button title="Enviar"/>
+        <Button title="Enviar" />
       </Form>
     </Container>
   )

49c27a01d122f2b2b46698a9f37d4d40f9b95475
Franklyn Rocha <faranresolve@gmail.com>
2022-05-17 09:48:04 -0300

    rotas e navegação por aba

--- App.tsx
+++ App.tsx
@@ -1,36 +1,39 @@
-import React from 'react';
-import AppLoading from 'expo-app-loading'
-import { ThemeProvider } from 'styled-components'
-import { StatusBar } from 'react-native'
+import React from "react";
+import AppLoading from "expo-app-loading";
+import { ThemeProvider } from "styled-components";
 
 import {
   useFonts,
   Poppins_400Regular,
   Poppins_500Medium,
-  Poppins_700Bold
-} from '@expo-google-fonts/poppins'
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
 
-import { Satisfy_400Regular } from '@expo-google-fonts/satisfy'
+import { Satisfy_400Regular } from "@expo-google-fonts/satisfy";
 
-import theme from './src/global/styles/theme'
-import { Register } from './src/screens/Register';
+import { NavigationContainer } from "@react-navigation/native";
+import { AppRoutes } from './src/routes/app.routes'
+
+import theme from "./src/global/styles/theme";
+import { Register } from "./src/screens/Register";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Poppins_400Regular,
     Poppins_500Medium,
     Poppins_700Bold,
-    Satisfy_400Regular
+    Satisfy_400Regular,
   });
 
   if (!fontsLoaded) {
-    return <AppLoading />    
+    return <AppLoading />;
   }
 
   return (
     <ThemeProvider theme={theme}>
-      <StatusBar translucent={true} backgroundColor={theme.colors.primary}/>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
-  )
+  );
 }
